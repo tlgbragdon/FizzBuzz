510d9113fe3914d3ffffdb69653d69a2d4e0508d
tlgbragdon <tracy.bragdon@gmail.com>
2016-09-10 16:07:09 -0400

    updated to use functions & improved validation

--- js/app.js
+++ js/app.js
@@ -1,46 +1,57 @@
 $(document).ready( function() {
 
-	var endNumber = 100;  // set default value
-	
-	//get value from user
-	$('form').submit (function (event) {
-		event.preventDefault();
+	var endNumber = 1; 
+
+	// if user input is valid, global variable endNumber is updated
+	function validPositiveInt(num) {
+		if (isNaN(num) || (num % 1 != 0) || (num <=0)) {
+			return false;		// Not a Number, or not an int, or not positive
+		};
+		endNumber = +num;
+		return true; 	
+	};
 		
-		// remove any previous output
-		$('p').remove ();
+	
+	// find and output FizzBuzz for numbers from 1 to num
+	function outputFizzBuzz (num){
 
-		endNumber = $('input').val();
-		// If x is Not a Number or less than one or greater than 10
-        if (isNaN(endNumber)) {
-        	endNumber=100;
-        	$('body').append ("<p> Invalid number entered; using value of 100 </p>");
-        };
-
-		// remove the submitted number from the form input
-		$(this)[0].reset();
-
-		//Loop thru and output fizz and/or buzz based on index value
-		for (index=1; index <= endNumber; index++)
-		{
+		// num has aleady been validated to be an integer
+		// Loop thru and output fizz and/or buzz based on index value
+		for (index=1; index <= num; index++) {
 			var outputString = index;
 			if (index % 3 ==0) {
 				outputString = "Fizz";
 			}
-
 			if (index % 5 == 0) {
 				if (outputString == index) {
 					outputString = "Buzz";
 				}
-				else // fizz is in there
-				{
+				else {   // fizz is in there
 					outputString += " " + "Buzz";
 				}
-				
 			}
-
-			$('body').append ("<p>" + outputString + "</p>");
-
+			$('.output').append ("<p>" + outputString + "</p>");
 		};
+	};
+
+	//get value from user
+	$('form').submit (function (event) {
+		event.preventDefault();
+
+		// remove any previous output
+		$('p').remove ();
+
+		//validate user input  - global var endNumber is updated if valid input provided	
+		if (validPositiveInt ($('input').val())){
+				outputFizzBuzz(endNumber);
+			}
+			else {
+        		$('.output').append ("<p> " + $('input').val() + " is not a valid positive integer; try again. </p>");
+        };
+       
+		// remove the submitted number from the form input
+		$(this)[0].reset();
+
 	});	
 
 });
